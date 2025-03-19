a07a6fa036ba39c20cc250836ac9b2d4b4fcc32e
1ZHOUJIAYI <150349546+1ZHOUJIAYI@users.noreply.github.com>
2025-03-19 19:34:00 +0800

    Add files via upload

---
+++ assignment4/4.1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+public class LinkedList<T>
+{
+    private class Node
+    {
+        public T Data { get; set; }
+        public Node Next { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+    private Node head;
+
+    public void Add(T data)
+    {
+        if (head == null)
+        {
+            head = new Node(data);
+        }
+        else
+        {
+            Node current = head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = new Node(data);
+        }
+    }
+
+    public void ForEach(Action<T> action)
+    {
+        Node current = head;
+        while (current != null)
+        {
+            action(current.Data);
+            current = current.Next;
+        }
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        LinkedList<int> list = new LinkedList<int>();
+        list.Add(3);
+        list.Add(21);
+        list.Add(340);
+        list.Add(45);
+        list.Add(59);
+
+        // 打印链表元素
+        Console.WriteLine("链表元素:");
+        list.ForEach(x => Console.WriteLine(x));
+
+        // 求最大值
+        int max = int.MinValue;
+        list.ForEach(x => { if (x > max) max = x; });
+        Console.WriteLine($"最大值: {max}");
+
+        // 求最小值
+        int min = int.MaxValue;
+        list.ForEach(x => { if (x < min) min = x; });
+        Console.WriteLine($"最小值: {min}");
+
+        // 求和
+        int sum = 0;
+        list.ForEach(x => sum += x);
+        Console.WriteLine($"和: {sum}");
+    }
+}
---
+++ assignment4/4.2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+
+public class AlarmClock
+{    public event EventHandler Tick;
+    public event EventHandler Alarm;
+
+    public DateTime CurrentTime { get; private set; }
+    public DateTime AlarmTime { get; set; }
+
+    public AlarmClock(DateTime alarmTime)
+    {
+        CurrentTime = DateTime.Now;
+        AlarmTime = alarmTime;
+    }
+
+   
+    public void Start()
+    {
+        Console.WriteLine("闹钟已启动！");
+        while (true)
+        {
+            CurrentTime = DateTime.Now;
+            Console.WriteLine($"当前时间: {CurrentTime:HH:mm:ss}");
+            OnTick();
+
+            if (CurrentTime >= AlarmTime)
+            {
+                OnAlarm();
+                break; 
+            }
+
+            Thread.Sleep(1000);
+        }
+    }
+
+
+    protected virtual void OnTick()
+    {
+        Tick?.Invoke(this, EventArgs.Empty);
+    }
+
+    protected virtual void OnAlarm()
+    {
+        Alarm?.Invoke(this, EventArgs.Empty);
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        DateTime alarmTime = DateTime.Now.AddSeconds(10);
+
+
+        AlarmClock alarmClock = new AlarmClock(alarmTime);
+
+        alarmClock.Tick += (sender, e) =>
+        {
+            Console.WriteLine("滴答...");
+        };
+
+        alarmClock.Alarm += (sender, e) =>
+        {
+            Console.WriteLine("叮铃铃！该起床了！");
+        };
+
+        alarmClock.Start();
+    }
+}
